23ea9918c2f53bf4dfd42a387890f8ba3c10b4f7
AKINDU-VIP <akindunewm@gmail.com>
2025-05-25 10:18:03 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let madeMenu = `*👋 ʜᴇʟʟᴏ ${pushname}* \n
- ❒ *ᴀᴋɪɴᴅᴜ ᴍᴅ ɴᴏᴡ ᴀʟɪᴠᴇ 😇* \n ❒ *ʜᴏᴡ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏᴀᴅʏ* \n ❒ *ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇᴛ ʙᴏᴛ ᴍᴇɴᴜ ᴛʏᴘᴇ .ᴍᴇɴᴜ 📜* \n\n ❒ *${day}* \n ❒ *${hour}* \n ❒ *${seconds}* 🧚‍♂ \n\n ❒ *ꜰᴏʟʟᴏᴡ ᴏᴜʀ ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ ꜰᴏʀ ᴀʙᴏᴜᴛ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇꜱ ᴀɴᴅ ᴛᴇᴄʜɴɪᴄᴀʟ ɪɴꜰᴏᴍᴀᴛɪᴏɴꜱ* \n\n https://whatsapp.com/channel/0029Vb5x0TSJP21B6svgXf2Z \n\n
+ ❒ *ᴀᴋɪɴᴅᴜ ᴍᴅ ᴀʟɪᴠᴇ ɴᴏᴡ 😇* \n ❒ *ʜᴏᴡ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏᴀᴅʏ* \n ❒ *ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇᴛ ʙᴏᴛ ᴍᴇɴᴜ ᴛʏᴘᴇ `.ᴍᴇɴᴜ` \n
 > *ᴀᴋɪɴᴅᴜ ᴍᴅ*`
 
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
